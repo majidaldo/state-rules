a8ec6506a3d48c9223ada4881d8c71a0e2280086
Majid alDosari <majidaldosari-github@yahoo.com>
2026-03-14 15:30:20 -0400

    try from main

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@ pkgs = [pkg]
 
 from subprocess import CalledProcessError
 def get_rev():
+    return 'main'
     from subprocess import check_output as run
     return run('git rev-parse --abbrev-ref HEAD', text=True, shell=True).strip()
 try:

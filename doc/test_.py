c3b0416ef67acf17e3d9ed396d43fdddc2770278
Majid alDosari <majidaldosari-github@yahoo.com>
2026-03-15 08:02:32 -0400

    simplify in a way that works for multiple argmaps for the same func

---
+++ doc/test_.py
@@ -0,0 +1,47 @@
+import marimo
+
+__generated_with = "0.24.0"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import state_rules.main as rm
+    _r = rm.Rules({'x':1})
+    @_r.register({
+        'x': 'x',
+         'y': 'x',
+        'return': 'x',})
+    def f1(x, y,):
+        return x+y
+
+    _r.run(5)
+    _r.log
+    return (rm,)
+
+
+@app.cell
+def _(rm):
+    # case for 'flexible' argmaps
+    from box import Box
+    # Instantiate the same ways as a regular dict
+    _s = Box({
+    'x':{'x': 1},
+    'z.y.x' : 3,
+    'y': 'yv',
+    },
+    default_box=True, box_dots=True)
+    _s = dict(_s.items(dotted=True))
+    _r = rm.Rules(_s)
+    # second argmap
+    @_r.register({'x': 'z.y.x', 'return': 'z.y.x.r' })
+    # first argmap
+    @_r.register({'x': 'x.x', 'return': 'x.x.r' })
+    def fd(x): return x+x
+    _r.run(5)
+    _r.log
+    return
+
+
+if __name__ == "__main__":
+    app.run()
--- src/state_rules/main.py
+++ src/state_rules/main.py
@@ -1,8 +1,9 @@
 class types:
-    state_keys = int | str
+    state_key = int | str # hashable?
     state = dict # can it be something else? just need mapping and iter
-    from typing import Callable
-    argmap = dict[state_keys, state_keys | Callable ]  # callable[state keys,-> ]
+    var = str
+    argmap = dict[var, state_key]
+
 
 class Rules:
 
@@ -11,55 +12,39 @@ class Rules:
         self.funcs = []
         self.log = [] if log is True else False
 
-    def add_func(self, f, argmap: types.argmap | None = None):
+    def add_func(self, f, argmap: types.argmap = {}):
         from inspect import signature
-        if argmap is None:
+        if not argmap:
             argmap = {p:p for p in signature(f).parameters}
         for s in signature(f).parameters:
             if s not in argmap:
                 argmap[s] = s
-        
         if 'return' not in argmap:
-            argmap['return'] = f'{f.__module__}.{f.__name__}()'#{','.join(v for v in argmap.values())})' doesnt work when v is callable
+            argmap['return'] = f'{f.__module__}.{f.__name__}()'
 
-        f.argmap = argmap
-        f._argmap_no_return = {fa:s  for fa,s in f.argmap.items() if (fa != 'return') }
-        self.funcs.append(f)
+        from types import SimpleNamespace as NS
+        _ = NS(
+            f = f,
+            argmap = {fa:sk  for fa,sk in argmap.items() if (fa != 'return') },
+            return_statekey = argmap['return'],)
+        self.funcs.append(_)
 
+    def register(self, argmap: types.argmap = {}):
+        def decorator(f):
+            self.add_func(f, argmap=argmap)
+            return f
+        return decorator
 
-    def register(self, argmap: types.argmap | None = None):
-        """decorator on a function"""
-        return lambda f: self.add_func(f, argmap=argmap)
-    
-    @classmethod
-    def _argmapf2args(cls, am: types.argmap, state: types.state):
-        for fa, s in am.items():
-            if callable(s):
-                for sk,sv in state.items():
-                    ssk = s(sk)
-                    if ssk == True:
-                        yield fa, sv
-                        break
-                    else: assert(ssk == False)
     
     def __iter__(self):
-        state = self.state
+        s = self.state
         for f in self.funcs:
-            _ = {fa:state[s]  for fa,s in f._argmap_no_return.items() if not callable(s) } # simple case
-            _.update(self._argmapf2args(f._argmap_no_return, state ) )
-            _ = f(**_)
-            rm = f.argmap['return']
-            if not callable(rm):
-                state[rm] = _
-            else:
-                assert(callable(rm))
-                for sk, sv in (state.copy() if self.i == 0 else state ).items():          # have to copy...
-                    rmsk = rm(sk)
-                    if rmsk == True:
-                        state[sk] = _                                                     # ...b/c this might change
-                    else: assert(rmsk == False)
-        yield state
+            _ = {a:s[sk] for a,sk in f.argmap.items() }
+            _ = f.f(**_)
+            s[f.return_statekey] = _
+        yield s
     
+
     def run(self, maxiter = 10):
         i = self.i = 0
         from types import SimpleNamespace as NS
